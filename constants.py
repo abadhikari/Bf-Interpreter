515aa2ee1dca115e7ba31dc159ce10cac56a82c0
Abhinna Adhikari <34376812+abadhikari@users.noreply.github.com>
2019-11-15 01:25:50 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 class Constants:
     """
-    Contains all the constants necessary for the Brainfuck Interpreter
+    Contains all the constants necessary for the Brainf*** Interpreter
     """
     TOTAL_MEMORY_CELLS = 30000
     MAX_NUM = 255
